00e3756830d37285ecd5c25c01c7847ef9ed475f
Viktoriah0307 <viktoriah0307@gmail.com>
2023-04-10 02:50:47 +0300

    fix

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,7 +2,7 @@ import throttle from 'lodash.throttle';
 
 const LOCAL_KEY = 'feedback-form-state';
 
-const form = document.querySelector('.feedback-form');
+form = document.querySelector('.feedback-form');
 
 form.addEventListener('input', throttle(onInputData, 500));
 form.addEventListener('submit', onFormSubmit);
@@ -17,7 +17,7 @@ function onInputData(e) {
 }
 
 function reloadPage() {
-  if (dataForm) {
+  if (dataForm){
     email.value = dataForm.email || '';
     message.value = dataForm.message || '';
   }
